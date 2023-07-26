effe690eb4d1fb05fd0db5d8a817d613503364dd
robertgeica <geicarobert@gmail.com>
2023-07-26 12:53:23 +0200

    init graphql express server

---
+++ app.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+const PORT = process.env.PORT || 5000;
+const BASE_URL = process.env.BASE_URL || 'http://localhost';
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const root = {
+  hello: () => {
+    return 'Hello world';
+  },
+};
+
+const app = express();
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: process.env.NODE_ENV === 'development',
+  })
+);
+
+app.listen(4000);
+
+console.log(`Running a GraphQL API server at ${BASE_URL}:${PORT}/graphql`);
