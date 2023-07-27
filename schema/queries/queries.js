71266a0d12c2e2f318e45405cda4940e01003c60
robertgeica <geicarobert@gmail.com>
2023-07-27 11:13:24 +0200

    add query to get logged in user notes

--- schema/queries/queries.js
+++ schema/queries/queries.js
@@ -36,6 +36,34 @@ const queries = new GraphQLObjectType({
         return Note.find({ visibility: { $ne: 'Private' } });
       },
     },
+    note: {
+      type: NoteType,
+      args: { id: { type: GraphQLID } },
+      resolve: async (parent, args, { userId }) => {
+        const note = await Note.findById(args.id);
+
+        if (note.visibility === 'Public') {
+          return note;
+        }
+        console.log(userId, note.userId);
+        if (!userId || userId !== note.userId.toString()) {
+          throw new Error(
+            'Unauthorized access. You must be the author to access a private note.'
+          );
+        }
+
+        return note;
+      },
+    },
+    userNotes: {
+      type: new GraphQLList(NoteType),
+      resolve(parent, args, { userId }) {
+        if(!userId) {
+          throw new Error("You must login first.")
+        }
+        return Note.find({ userId: { $eq: userId } });
+      },
+    }
   },
 });
 
